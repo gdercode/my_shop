582dd9b6a6cfd5eb62b15038009841aac87e1421
Gder Code <gdercode@gmail.com>
2025-04-06 13:50:55 +0200

    layout codes

--- resources/views/home_page.blade.php
+++ resources/views/home_page.blade.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>home_page</title>
-</head>
-
-<body>
+@extends('layouts.main_layout')
+@section('content')
     <h1>home page</h1>
-</body>
-
-</html>
+@endsection
---
+++ resources/views/layouts/main_layout.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>E-Commerce</title>
+    <link rel="stylesheet" href="{{ asset('css/main_layout.css') }}">
+</head>
+
+<body>
+
+    <nav class="navbar">
+        <div class="container">
+            <a class="brand" href="{{ route('home_page') }}">E-Commerce</a>
+            <ul class="menu">
+                <li><a href="">Home</a></li>
+                <li><a href="">New Product</a></li>
+                <li><a href="">logout</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container">
+        @auth
+            <b class="success"> {{ auth()->user()->name }} </b>
+        @endauth
+        @if ($errors->any())
+            <div class="error">
+                <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        @if (session('success'))
+            <p class="success">{{ session('success') }}</p>
+        @endif
+        @yield('content')
+    </div>
+
+</body>
+
+</html>
